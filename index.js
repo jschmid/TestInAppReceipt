41217093953869472c7d71328c21d37ecc2d0378
Jonas Schmid <jonas.schmid@gmail.com>
2014-10-27 16:36:36 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,41 @@
+var express = require('express')
+var app = express()
+var bodyParser = require('body-parser');
+var IAPVerifier = require('iap_verifier');
+var client = new IAPVerifier();
+
+app.use(bodyParser.json())
+
+app.get('/', function(req, res) {
+	res.json({
+		'Hello': 'World!'
+	})
+})
+
+app.post('/', function(req, res) {
+	console.log(req.body);
+	var receipt = req.body['receipt'];
+	client.verifyAutoRenewReceipt(receipt, true, function(valid, msg, data) {
+		if (valid) {
+			console.log("Valid receipt");
+			console.log(msg);
+			console.log(data);
+			res.json({'res':'ok'});
+		} else {
+			console.log("Invalid receipt");
+			console.log(msg);
+			console.log(data);
+			res.status(400).end();
+		}
+	});
+
+})
+
+var server = app.listen(3000, function() {
+
+	var host = server.address().address
+	var port = server.address().port
+
+	console.log('Example app listening at http://%s:%s', host, port)
+
+})
